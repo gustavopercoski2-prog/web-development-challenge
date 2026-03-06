e9e2da44dcf224426199a5ac760e996d331a1c88
Gustavo Percoski <gustavopercoski2@gmail.com>
2026-03-06 07:58:01 -0300

    feat: implementa JS para efeito de blur no scroll da navbar

---
+++ js/home.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // --- Efeito de scroll navbar ---
+    const header = document.getElementById('header');
+    
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    }, { passive: true });
+});
